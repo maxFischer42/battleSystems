0a2cbafe6860fb4c55ac7ad89945167665f80ed4
maxFischer42 <42ducksable1@gmail.com>
2018-08-17 17:13:15 -0700

    Initialized the Unity project

---
+++ PrototypeBattleSystems/Assets/Grid Turn Based/Scripts/createGrid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createGrid : MonoBehaviour {
+
+    public Vector2 gridDimensions;
+    public Vector2 NumberOfEnemies;
+    public Vector2[] EnemyStartPositions;
+    public Vector2 PlayerStartPoitions;
+    public GameObject gridObject
+
+    private void Awake()
+    {
+                        
+    }
+}
